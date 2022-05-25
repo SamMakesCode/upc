67d938e4f86a46749b96f83be1f11c063c2c653c
Sam Littler <samlittler57@gmail.com>
2022-05-25 10:35:58 +0100

    Update to >= php 8.0

--- src/UPC.php
+++ src/UPC.php
@@ -4,39 +4,35 @@ namespace SamLittler\UPC;
 
 class UPC
 {
-    public static function calculate($data)
+    public static function calculate(string $data): int
     {
-        if (!is_string($data)) {
-            throw new \InvalidArgumentException('Data with value "' . $data . '" is not a string.');
-        }
-
         // Get all the individual digits
         $digits = str_split($data);
 
         // Create variables to store sums
-        $even_digit_sum = 0;
-        $odd_digit_sum = 0;
+        $evenDigitSum = 0;
+        $oddDigitSum = 0;
 
         // For each digit
         // Bear in mind, we start counting from 0, so odd and even operations are swapped here
         for ($i = 0; $i < count($digits); $i++) {
             if ($i % 2 === 0) {
                 // If it's even
-                $odd_digit_sum += $digits[$i];
+                $oddDigitSum += $digits[$i];
             } else {
                 // If it's odd
-                $even_digit_sum += $digits[$i];
+                $evenDigitSum += $digits[$i];
             }
         }
 
         // Multiply the sum of the odd digits by 3
-        $odd_digit_sum_multiplied = $odd_digit_sum * 3;
+        $oddDigitSumMultiplied = $oddDigitSum * 3;
 
         // Add the sums together
-        $sum_of_both = $odd_digit_sum_multiplied + $even_digit_sum;
+        $SumOfBoth = $oddDigitSumMultiplied + $evenDigitSum;
 
         // Get the remainder when divided by 10
-        $remainder = $sum_of_both % 10;
+        $remainder = $SumOfBoth % 10;
 
         // If it's not 0, minus it from 10
         if ($remainder !== 0) {
--- tests/UPCTest.php
+++ tests/UPCTest.php
@@ -5,10 +5,10 @@ class UPCTest extends \PHPUnit\Framework\TestCase
     public function testCalculatesCorrectly()
     {
         $data = [
-            ['check' => '7', 'string' => '03600024145'],
-            ['check' => '1', 'string' => '69277198116'],
-            ['check' => '5', 'string' => '72641217542'],
-            ['check' => '7', 'string' => '05432122345'],
+            ['check' => 7, 'string' => '03600024145'],
+            ['check' => 1, 'string' => '69277198116'],
+            ['check' => 5, 'string' => '72641217542'],
+            ['check' => 7, 'string' => '05432122345'],
         ];
 
         foreach ($data as $row) {
